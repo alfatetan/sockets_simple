3300388e2f6b1408d2924513828c043564a8dd2b
alfatetan <alfatetan@gmail.com>
2019-09-06 11:51:08 +0500

    Change name

--- sample_server.py
+++ sample_server.py
@@ -1,51 +1,47 @@
-from time import sleep
-import socket
-import socketserver
+#!/usr/bin/python3.7
+# The module for managing the sample_server
 
-class Stream(object):
-    """
-    Reading the book by words for stream generation
-    """
+import sys
 
-    def __init__(self, bookname):
-        """
-        Open the book and generate the words
-        :param bookname: path to txt file with book
-        """
-        with open(bookname, 'r') as book:
-            self.all_words = book.read().split()
-        return
 
-    def get_word(self):
-        return self.all_words.pop(0)
+def man_help():
+    print('Attention! The sample_server must be work before to use next '
+          'commands.\nFor start work the sample_server you need load the '
+          'sample_server into memory with command:\n'
+          'python3.7 sample_server.py &\n'
+          'or, you can use command --start this server_manager:\n'
+          'python3 server_manager --start')
+    print('--help\tthis help page')
+    print('--start\tload the sample_server into memory and start standby mode')
+    print('--run\tstart work the ')
+    return
 
-    def get_words(self, value=1, wait=0):
-        """
-        Generating words from book
-        :param words: how much words you need take from book. Default = 1
-        :return: several words from book
-        """
-        while value:
-            sleep(wait)
-            yield self.get_word()
-            value -= 1
-        return
 
-    def stream(self, wait=.300):
-        """
-        Generating words from stream
-        :return: stream
-        """
-        while self.all_words.__len__():
-            sleep(wait)
-            yield self.get_word()
+def server_run():
+    print('server_run')
+    return
+
+
+def server_stop():
+    print('server_stop')
+    return
+
+
+def server_status():
+    print('server_status')
+    return
 
 
 if __name__ == '__main__':
-    stream = Stream('book.txt')
-    # for w in stream.stream():
-    #     print(w)
-    srv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    print(srv_socket.fileno())
-    srv_socket.accept()
-    srv_socket.recv(1)
+    commands = {
+                   '--help': man_help,
+                   '--start': server_run,
+                   '--stop': server_stop,
+                   '--status': server_status,
+                    '--run': server_run,
+               }
+
+    for arg in sys.argv:
+        if commands.get(arg, False):
+            commands[arg]()
+
